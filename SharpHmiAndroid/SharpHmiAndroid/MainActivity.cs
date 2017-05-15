bf8b05d1e4b2ea6016d9c68eb665ec482d5da160
Anil Dahiya <anil_dahiya@infosys.com>
2017-05-15 16:26:02 +0530

    Changes for adding Navigation and Action bar to Android Hmi app. Further fragments need to be added for displaying up the registered apps and their full view.

--- SharpHmiAndroid/SharpHmiAndroid/MainActivity.cs
+++ SharpHmiAndroid/SharpHmiAndroid/MainActivity.cs
@@ -1,14 +1,18 @@
 ﻿using Android.App;
-using Android.Widget;
 using Android.OS;
+using Android.Support.V7.App;
+using Android.Support.Design.Widget;
+using Android.Support.V4.Widget;
+using Android.Support.V7.Widget;
+using Android.Views;
 
 namespace SharpHmiAndroid
 {
-	[Activity(Label = "SharpHmiAndroid", MainLauncher = true, Icon = "@mipmap/icon")]
-	public class MainActivity : Activity
+	[Activity(Label = "SharpHmiAndroid", MainLauncher = true)]
+	public class MainActivity : AppCompatActivity
 	{
-		int count = 1;
-
+		NavigationView navigationView;
+		DrawerLayout drawer;
 		protected override void OnCreate(Bundle savedInstanceState)
 		{
 			base.OnCreate(savedInstanceState);
@@ -16,14 +20,59 @@ namespace SharpHmiAndroid
 			// Set our view from the "main" layout resource
 			SetContentView(Resource.Layout.Main);
 
-			// Get our button from the layout resource,
-			// and attach an event to it
-			Button button = FindViewById<Button>(Resource.Id.myButton);
+			var toolbar = FindViewById<Toolbar>(Resource.Id.toolbar);
+			SetSupportActionBar(toolbar);
+            SupportActionBar.SetHomeButtonEnabled(true);  
+			SupportActionBar.SetHomeAsUpIndicator(Resource.Drawable.ic_menu);
 
-			button.Click += delegate { button.Text = $"{count++} clicks!"; };
+			SupportActionBar.SetDisplayHomeAsUpEnabled(true);
 
-			CoreListener coreListener = new CoreListener("192.168.1.213", 8087);
-			coreListener.setupConnection();
+			drawer = FindViewById<DrawerLayout>(Resource.Id.drawer_layout);
+
+			ActionBarDrawerToggle _actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawer, toolbar, Resource.String.navigation_drawer_open,
+			Resource.String.navigation_drawer_close);
+
+			drawer.SetDrawerListener(_actionBarDrawerToggle);
+
+			navigationView = FindViewById<NavigationView>(Resource.Id.nav_view);
+			navigationView.NavigationItemSelected += OnNavigationItemSelected;
+		}
+
+		private void OnNavigationItemSelected(object sender, NavigationView.NavigationItemSelectedEventArgs e)
+		{
+			var menuItem = e.MenuItem;
+			menuItem.SetChecked(!menuItem.IsChecked);
+			drawer.CloseDrawers();
+
+			switch (menuItem.ItemId)
+			{
+				case Resource.Id.consoleLogs:
+					Android.Widget.Toast.MakeText(Application.Context, "Console Logs selected", Android.Widget.ToastLength.Long).Show();
+					break;
+
+				case Resource.Id.findApps:
+					Android.Widget.Toast.MakeText(Application.Context, "Find New Apps selected", Android.Widget.ToastLength.Long).Show();
+					break;
+
+				case Resource.Id.nav_exit:
+					Android.Widget.Toast.MakeText(Application.Context, "Exit App selected", Android.Widget.ToastLength.Long).Show();
+					break;
+
+				default:
+					Android.Widget.Toast.MakeText(Application.Context, "Something Wrong", Android.Widget.ToastLength.Long).Show();
+					break;
+
+			}		}
+
+		public override bool OnOptionsItemSelected(IMenuItem item)
+		{
+			switch (item.ItemId)
+			{
+				case Android.Resource.Id.Home:
+					drawer.OpenDrawer(Android.Support.V4.View.GravityCompat.Start);
+					return true;
+			}
+			return base.OnOptionsItemSelected(item);
 		}
 	}
 }
